e16854304b02f0aa553d427eff03a1c94216af74
vemis <micsindler@gmail.com>
2026-04-20 22:18:36 +0200

    MongoDB Mongoose JavaScript refactored loaders so that they are more human readable

--- evaluation/microservice-mongodb-mongoose-javascript/src/loader/loaderE.js
+++ evaluation/microservice-mongodb-mongoose-javascript/src/loader/loaderE.js
@@ -26,90 +26,174 @@ async function insertBatched(Model, docs, batchSize = BATCH) {
     }
 }
 
-async function loadAll(dataPath) {
-    const p = f => path.join(dataPath, f);
-
-    const ordersData   = await readDataFromCustomSeparator(p('orders.tbl'));
-    const lineitemsData = await readDataFromCustomSeparator(p('lineitem.tbl'));
-
-    // ordersEWithLineitems
+async function loadOrdersEWithLineitems(filePathOrders, filePathLineitems) {
     console.log('Loading ordersEWithLineitems...');
+
+    const lineitemsData = await readDataFromCustomSeparator(filePathLineitems);
+
     const lineitemsByOrder = new Map();
     for (const row of lineitemsData) {
         const key = Number(row[0]);
         if (!lineitemsByOrder.has(key)) lineitemsByOrder.set(key, []);
         lineitemsByOrder.get(key).push({
             l_id: row[0] + row[3],
-            l_orderkey: Number(row[0]), l_partkey: Number(row[1]), l_suppkey: Number(row[2]),
+            l_orderkey: Number(row[0]),
+            l_partkey: Number(row[1]),
+            l_suppkey: Number(row[2]),
             l_ps_id: row[1] + '|' + row[2],
-            l_linenumber: Number(row[3]), l_quantity: Number(row[4]),
-            l_extendedprice: Number(row[5]), l_discount: Number(row[6]), l_tax: Number(row[7]),
-            l_returnflag: row[8], l_linestatus: row[9],
-            l_shipdate: new Date(row[10]), l_commitdate: new Date(row[11]), l_receiptdate: new Date(row[12]),
-            l_shipinstruct: row[13], l_shipmode: row[14], l_comment: row[15]
+            l_linenumber: Number(row[3]),
+            l_quantity: Number(row[4]),
+            l_extendedprice: Number(row[5]),
+            l_discount: Number(row[6]),
+            l_tax: Number(row[7]),
+            l_returnflag: row[8],
+            l_linestatus: row[9],
+            l_shipdate: new Date(row[10]),
+            l_commitdate: new Date(row[11]),
+            l_receiptdate: new Date(row[12]),
+            l_shipinstruct: row[13],
+            l_shipmode: row[14],
+            l_comment: row[15]
         });
     }
-    const ordersWithLineitems = ordersData.map(row => ({
-        _id: Number(row[0]), o_custkey: Number(row[1]), o_orderstatus: row[2], o_totalprice: row[3],
-        o_orderdate: new Date(row[4]), o_orderpriority: row[5], o_clerk: row[6], o_shippriority: row[7], o_comment: row[8],
+
+    const ordersData = await readDataFromCustomSeparator(filePathOrders);
+
+    const rowsOfSchemas = ordersData.map(row => ({
+        _id: Number(row[0]),
+        o_custkey: Number(row[1]),
+        o_orderstatus: row[2],
+        o_totalprice: row[3],
+        o_orderdate: new Date(row[4]),
+        o_orderpriority: row[5],
+        o_clerk: row[6],
+        o_shippriority: row[7],
+        o_comment: row[8],
         o_lineitems: lineitemsByOrder.get(Number(row[0])) ?? []
     }));
-    await insertBatched(OrdersEWithLineitems, ordersWithLineitems);
 
-    // ordersEWithLineitemsArrayAsTags
+    await insertBatched(OrdersEWithLineitems, rowsOfSchemas);
+    console.log('ordersEWithLineitems loaded');
+}
+
+async function loadOrdersEWithLineitemsArrayAsTags(filePathOrders, filePathLineitems) {
     console.log('Loading ordersEWithLineitemsArrayAsTags...');
-    const lineitemRow2Tags = createLineitemsTags(lineitemsData[1]);
-    const ordersAsTags = ordersData.map(row => ({
+
+    const ordersData    = await readDataFromCustomSeparator(filePathOrders);
+    const lineitemsData = await readDataFromCustomSeparator(filePathLineitems);
+
+    const lineitemRow2 = lineitemsData[1];
+
+    const rowsOfSchemas = ordersData.map(row => ({
         _id: Number(row[0]),
         o_orderdate: new Date(row[4]),
-        o_lineitems_tags: shuffleAndTruncate(lineitemRow2Tags, Number(row[0]))
+        o_lineitems_tags: shuffleAndTruncate(createLineitemsTags(lineitemRow2), Number(row[0]))
     }));
-    await insertBatched(OrdersEWithLineitemsArrayAsTags, ordersAsTags);
 
-    // ordersEWithLineitemsArrayAsTagsIndexed
+    await insertBatched(OrdersEWithLineitemsArrayAsTags, rowsOfSchemas);
+    console.log('ordersEWithLineitemsArrayAsTags loaded');
+}
+
+async function loadOrdersEWithLineitemsArrayAsTagsIndexed(filePathOrders, filePathLineitems) {
     console.log('Loading ordersEWithLineitemsArrayAsTagsIndexed...');
-    const ordersAsTagsIndexed = ordersData.map(row => ({
+
+    const ordersData    = await readDataFromCustomSeparator(filePathOrders);
+    const lineitemsData = await readDataFromCustomSeparator(filePathLineitems);
+
+    const lineitemRow2 = lineitemsData[1];
+
+    const rowsOfSchemas = ordersData.map(row => ({
         _id: Number(row[0]),
         o_orderdate: new Date(row[4]),
-        o_lineitems_tags_indexed: shuffleAndTruncate(lineitemRow2Tags, Number(row[0]))
+        o_lineitems_tags_indexed: shuffleAndTruncate(createLineitemsTags(lineitemRow2), Number(row[0]))
     }));
-    await insertBatched(OrdersEWithLineitemsArrayAsTagsIndexed, ordersAsTagsIndexed);
 
-    // ordersEWithCustomerWithNationWithRegion
+    await insertBatched(OrdersEWithLineitemsArrayAsTagsIndexed, rowsOfSchemas);
+    console.log('ordersEWithLineitemsArrayAsTagsIndexed loaded');
+}
+
+async function loadOrdersEWithCustomerWithNationWithRegion(filePathOrders, filePathCustomers, filePathNations, filePathRegions) {
     console.log('Loading ordersEWithCustomerWithNationWithRegion...');
-    const regionRows   = await readDataFromCustomSeparator(p('region.tbl'));
-    const nationRows   = await readDataFromCustomSeparator(p('nation.tbl'));
-    const customerRows = await readDataFromCustomSeparator(p('customer.tbl'));
 
-    const regionMap = new Map(regionRows.map(r => [Number(r[0]), { r_regionkey: Number(r[0]), r_name: r[1] }]));
+    const regionRows   = await readDataFromCustomSeparator(filePathRegions);
+    const nationRows   = await readDataFromCustomSeparator(filePathNations);
+    const customerRows = await readDataFromCustomSeparator(filePathCustomers);
+    const ordersData   = await readDataFromCustomSeparator(filePathOrders);
+
+    const regionMap = new Map(regionRows.map(r => [
+        Number(r[0]),
+        { r_regionkey: Number(r[0]), r_name: r[1] }
+    ]));
+
     const nationMap = new Map(nationRows.map(r => {
         const n_regionkey = Number(r[2]);
-        return [Number(r[0]), { n_nationkey: Number(r[0]), n_name: r[1], n_regionkey, n_region: regionMap.get(n_regionkey) }];
+        return [Number(r[0]), {
+            n_nationkey: Number(r[0]),
+            n_name: r[1],
+            n_regionkey,
+            n_region: regionMap.get(n_regionkey)
+        }];
     }));
+
     const customerMap = new Map(customerRows.map(r => {
         const c_nationkey = Number(r[3]);
-        return [Number(r[0]), { c_custkey: Number(r[0]), c_name: r[1], c_nationkey, c_nation: nationMap.get(c_nationkey) }];
+        return [Number(r[0]), {
+            c_custkey: Number(r[0]),
+            c_name: r[1],
+            c_nationkey,
+            c_nation: nationMap.get(c_nationkey)
+        }];
     }));
-    const ordersWithCustomer = ordersData.map(row => ({
+
+    const rowsOfSchemas = ordersData.map(row => ({
         _id: Number(row[0]),
         o_orderdate: new Date(row[4]),
         o_customer: customerMap.get(Number(row[1]))
     }));
-    await insertBatched(OrdersEWithCustomerWithNationWithRegion, ordersWithCustomer);
 
-    // ordersEOnlyOComment
+    await insertBatched(OrdersEWithCustomerWithNationWithRegion, rowsOfSchemas);
+    console.log('ordersEWithCustomerWithNationWithRegion loaded');
+}
+
+async function loadOrdersEOnlyOComment(filePathOrders) {
     console.log('Loading ordersEOnlyOComment...');
-    const ordersOnlyComment = ordersData.map(row => ({
-        _id: Number(row[0]), o_orderdate: new Date(row[4]), o_comment: row[8]
-    }));
-    await insertBatched(OrdersEOnlyOComment, ordersOnlyComment);
 
-    // ordersEOnlyOCommentIndexed
-    console.log('Loading ordersEOnlyOCommentIndexed...');
-    const ordersOnlyCommentIndexed = ordersData.map(row => ({
-        _id: Number(row[0]), o_orderdate: new Date(row[4]), o_comment: row[8]
+    const ordersData = await readDataFromCustomSeparator(filePathOrders);
+
+    const rowsOfSchemas = ordersData.map(row => ({
+        _id: Number(row[0]),
+        o_orderdate: new Date(row[4]),
+        o_comment: row[8]
     }));
-    await insertBatched(OrdersEOnlyOCommentIndexed, ordersOnlyCommentIndexed);
+
+    await insertBatched(OrdersEOnlyOComment, rowsOfSchemas);
+    console.log('ordersEOnlyOComment loaded');
+}
+
+async function loadOrdersEOnlyOCommentIndexed(filePathOrders) {
+    console.log('Loading ordersEOnlyOCommentIndexed...');
+
+    const ordersData = await readDataFromCustomSeparator(filePathOrders);
+
+    const rowsOfSchemas = ordersData.map(row => ({
+        _id: Number(row[0]),
+        o_orderdate: new Date(row[4]),
+        o_comment: row[8]
+    }));
+
+    await insertBatched(OrdersEOnlyOCommentIndexed, rowsOfSchemas);
+    console.log('ordersEOnlyOCommentIndexed loaded');
+}
+
+async function loadAll(dataPath) {
+    const p = f => path.join(dataPath, f);
+
+    await loadOrdersEWithLineitems(p('orders.tbl'), p('lineitem.tbl'));
+    await loadOrdersEWithLineitemsArrayAsTags(p('orders.tbl'), p('lineitem.tbl'));
+    await loadOrdersEWithLineitemsArrayAsTagsIndexed(p('orders.tbl'), p('lineitem.tbl'));
+    await loadOrdersEWithCustomerWithNationWithRegion(p('orders.tbl'), p('customer.tbl'), p('nation.tbl'), p('region.tbl'));
+    await loadOrdersEOnlyOComment(p('orders.tbl'));
+    await loadOrdersEOnlyOCommentIndexed(p('orders.tbl'));
 }
 
 export async function runEmbeddedLoader() {
--- evaluation/microservice-mongodb-mongoose-javascript/src/loader/loaderR.js
+++ evaluation/microservice-mongodb-mongoose-javascript/src/loader/loaderR.js
@@ -20,65 +20,221 @@ async function insertBatched(Model, docs, batchSize = BATCH) {
     }
 }
 
+async function loadRegions(filePath) {
+    const rowsOfData = await readDataFromCustomSeparator(filePath);
+
+    const rowsOfSchemas = rowsOfData.map(([_id, r_name, r_comment]) => ({
+        _id: Number(_id),
+        r_name,
+        r_comment
+    }));
+
+    await RegionR.insertMany(rowsOfSchemas);
+    console.log('regionR loaded');
+}
+
+async function loadNations(filePath) {
+    const rowsOfData = await readDataFromCustomSeparator(filePath);
+
+    const rowsOfSchemas = rowsOfData.map(([_id, n_name, n_regionkey, n_comment]) => ({
+        _id: Number(_id),
+        n_name,
+        n_regionkey: Number(n_regionkey),
+        n_comment
+    }));
+
+    await NationR.insertMany(rowsOfSchemas);
+    console.log('nationR loaded');
+}
+
+async function loadCustomers(filePath) {
+    const rowsOfData = await readDataFromCustomSeparator(filePath);
+
+    const rowsOfSchemas = rowsOfData.map(([
+        _id,
+        c_name,
+        c_address,
+        c_nationkey,
+        c_phone,
+        c_acctbal,
+        c_mktsegment,
+        c_commen
+    ]) => ({
+        _id: Number(_id),
+        c_name,
+        c_address,
+        c_nationkey: Number(c_nationkey),
+        c_phone,
+        c_acctbal: Number(c_acctbal),
+        c_mktsegment,
+        c_commen
+    }));
+
+    await insertBatched(CustomerR, rowsOfSchemas);
+    console.log('customerR loaded');
+}
+
+async function loadOrders(filePath) {
+    const rowsOfData = await readDataFromCustomSeparator(filePath);
+
+    const rowsOfSchemas = rowsOfData.map(([
+        _id,
+        o_custkey,
+        o_orderstatus,
+        o_totalprice,
+        o_orderdate,
+        o_orderpriority,
+        o_clerk,
+        o_shippriority,
+        o_comment
+    ]) => ({
+        _id: Number(_id),
+        o_custkey: Number(o_custkey),
+        o_orderstatus,
+        o_totalprice,
+        o_orderdate: new Date(o_orderdate),
+        o_orderpriority,
+        o_clerk,
+        o_shippriority,
+        o_comment
+    }));
+
+    await insertBatched(OrdersR, rowsOfSchemas);
+    console.log('ordersR loaded');
+}
+
+async function loadLineitems(filePath) {
+    const rowsOfData = await readDataFromCustomSeparator(filePath);
+
+    const rowsOfSchemas = rowsOfData.map(([
+        l_orderkey,
+        l_partkey,
+        l_suppkey,
+        l_linenumber,
+        l_quantity,
+        l_extendedprice,
+        l_discount,
+        l_tax,
+        l_returnflag,
+        l_linestatus,
+        l_shipdate,
+        l_commitdate,
+        l_receiptdate,
+        l_shipinstruct,
+        l_shipmode,
+        l_comment
+    ]) => ({
+        _id: l_orderkey + '|' + l_linenumber,
+        l_orderkey: Number(l_orderkey),
+        l_partkey: Number(l_partkey),
+        l_suppkey: Number(l_suppkey),
+        l_ps_id: l_partkey + '|' + l_suppkey,
+        l_linenumber: Number(l_linenumber),
+        l_quantity: Number(l_quantity),
+        l_extendedprice: Number(l_extendedprice),
+        l_discount: Number(l_discount),
+        l_tax: Number(l_tax),
+        l_returnflag,
+        l_linestatus,
+        l_shipdate: new Date(l_shipdate),
+        l_commitdate: new Date(l_commitdate),
+        l_receiptdate: new Date(l_receiptdate),
+        l_shipinstruct,
+        l_shipmode,
+        l_comment
+    }));
+
+    await insertBatched(LineitemR, rowsOfSchemas);
+    console.log('lineitemR loaded');
+}
+
+async function loadPartsupps(filePath) {
+    const rowsOfData = await readDataFromCustomSeparator(filePath);
+
+    const rowsOfSchemas = rowsOfData.map(([
+        ps_partkey,
+        ps_suppkey,
+        ps_availqty,
+        ps_supplycost,
+        ps_comment
+    ]) => ({
+        _id: ps_partkey + '|' + ps_suppkey,
+        ps_partkey: Number(ps_partkey),
+        ps_suppkey: Number(ps_suppkey),
+        ps_availqty: Number(ps_availqty),
+        ps_supplycost: Number(ps_supplycost),
+        ps_comment
+    }));
+
+    await insertBatched(PartsuppR, rowsOfSchemas);
+    console.log('partsuppR loaded');
+}
+
+async function loadSuppliers(filePath) {
+    const rowsOfData = await readDataFromCustomSeparator(filePath);
+
+    const rowsOfSchemas = rowsOfData.map(([
+        _id,
+        s_name,
+        s_address,
+        s_nationkey,
+        s_phone,
+        s_acctbal,
+        s_comment
+    ]) => ({
+        _id: Number(_id),
+        s_name,
+        s_address,
+        s_nationkey: Number(s_nationkey),
+        s_phone,
+        s_acctbal: Number(s_acctbal),
+        s_comment
+    }));
+
+    await insertBatched(SupplierR, rowsOfSchemas);
+    console.log('supplierR loaded');
+}
+
+async function loadParts(filePath) {
+    const rowsOfData = await readDataFromCustomSeparator(filePath);
+
+    const rowsOfSchemas = rowsOfData.map(([
+        _id,
+        p_name,
+        p_mfgr,
+        p_brand,
+        p_type,
+        p_size,
+        p_container,
+        p_retailprice,
+        p_commen
+    ]) => ({
+        _id: Number(_id),
+        p_name,
+        p_mfgr,
+        p_brand,
+        p_type,
+        p_size: Number(p_size),
+        p_container,
+        p_retailprice: Number(p_retailprice),
+        p_commen
+    }));
+
+    await insertBatched(PartR, rowsOfSchemas);
+    console.log('partR loaded');
+}
+
 async function loadAll(dataPath) {
     const p = f => path.join(dataPath, f);
 
-    const regions = (await readDataFromCustomSeparator(p('region.tbl'))).map(([_id, r_name, r_comment]) => ({
-        _id: Number(_id), r_name, r_comment
-    }));
-    await RegionR.insertMany(regions);
-    console.log('regionR loaded');
-
-    const nations = (await readDataFromCustomSeparator(p('nation.tbl'))).map(([_id, n_name, n_regionkey, n_comment]) => ({
-        _id: Number(_id), n_name, n_regionkey: Number(n_regionkey), n_comment
-    }));
-    await NationR.insertMany(nations);
-    console.log('nationR loaded');
-
-    const customers = (await readDataFromCustomSeparator(p('customer.tbl'))).map(([_id, c_name, c_address, c_nationkey, c_phone, c_acctbal, c_mktsegment, c_commen]) => ({
-        _id: Number(_id), c_name, c_address, c_nationkey: Number(c_nationkey), c_phone, c_acctbal: Number(c_acctbal), c_mktsegment, c_commen
-    }));
-    await insertBatched(CustomerR, customers);
-    console.log('customerR loaded');
-
-    const orders = (await readDataFromCustomSeparator(p('orders.tbl'))).map(([_id, o_custkey, o_orderstatus, o_totalprice, o_orderdate, o_orderpriority, o_clerk, o_shippriority, o_comment]) => ({
-        _id: Number(_id), o_custkey: Number(o_custkey), o_orderstatus, o_totalprice, o_orderdate: new Date(o_orderdate), o_orderpriority, o_clerk, o_shippriority, o_comment
-    }));
-    await insertBatched(OrdersR, orders);
-    console.log('ordersR loaded');
-
-    const lineitems = (await readDataFromCustomSeparator(p('lineitem.tbl'))).map(([l_orderkey, l_partkey, l_suppkey, l_linenumber, l_quantity, l_extendedprice, l_discount, l_tax, l_returnflag, l_linestatus, l_shipdate, l_commitdate, l_receiptdate, l_shipinstruct, l_shipmode, l_comment]) => ({
-        _id: l_orderkey + '|' + l_linenumber,
-        l_orderkey: Number(l_orderkey), l_partkey: Number(l_partkey), l_suppkey: Number(l_suppkey),
-        l_ps_id: l_partkey + '|' + l_suppkey,
-        l_linenumber: Number(l_linenumber), l_quantity: Number(l_quantity),
-        l_extendedprice: Number(l_extendedprice), l_discount: Number(l_discount), l_tax: Number(l_tax),
-        l_returnflag, l_linestatus,
-        l_shipdate: new Date(l_shipdate), l_commitdate: new Date(l_commitdate), l_receiptdate: new Date(l_receiptdate),
-        l_shipinstruct, l_shipmode, l_comment
-    }));
-    await insertBatched(LineitemR, lineitems);
-    console.log('lineitemR loaded');
-
-    const partsupps = (await readDataFromCustomSeparator(p('partsupp.tbl'))).map(([ps_partkey, ps_suppkey, ps_availqty, ps_supplycost, ps_comment]) => ({
-        _id: ps_partkey + '|' + ps_suppkey,
-        ps_partkey: Number(ps_partkey), ps_suppkey: Number(ps_suppkey),
-        ps_availqty: Number(ps_availqty), ps_supplycost: Number(ps_supplycost), ps_comment
-    }));
-    await insertBatched(PartsuppR, partsupps);
-    console.log('partsuppR loaded');
-
-    const suppliers = (await readDataFromCustomSeparator(p('supplier.tbl'))).map(([_id, s_name, s_address, s_nationkey, s_phone, s_acctbal, s_comment]) => ({
-        _id: Number(_id), s_name, s_address, s_nationkey: Number(s_nationkey), s_phone, s_acctbal: Number(s_acctbal), s_comment
-    }));
-    await insertBatched(SupplierR, suppliers);
-    console.log('supplierR loaded');
-
-    const parts = (await readDataFromCustomSeparator(p('part.tbl'))).map(([_id, p_name, p_mfgr, p_brand, p_type, p_size, p_container, p_retailprice, p_commen]) => ({
-        _id: Number(_id), p_name, p_mfgr, p_brand, p_type, p_size: Number(p_size), p_container, p_retailprice: Number(p_retailprice), p_commen
-    }));
-    await insertBatched(PartR, parts);
-    console.log('partR loaded');
+    await loadRegions(p('region.tbl'));
+    await loadNations(p('nation.tbl'));
+    await loadCustomers(p('customer.tbl'));
+    await loadOrders(p('orders.tbl'));
+    await loadLineitems(p('lineitem.tbl'));
+    await loadPartsupps(p('partsupp.tbl'));
+    await loadSuppliers(p('supplier.tbl'));
+    await loadParts(p('part.tbl'));
 }
 
 export async function runRelationalLoader() {
